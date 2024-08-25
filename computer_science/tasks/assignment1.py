e13552c2ed789ce291067451516167c805611b22
Leighton Chen <chenletong19@gmail.com>
2024-08-25 20:58:21 +0800

    update computer science codes

--- computer_science/tasks/assignment1.py
+++ computer_science/tasks/assignment1.py
@@ -3,18 +3,11 @@
 
 x = 5 #variable assignment
 y = 10
-
-def v1():
-    global y
-    print (y)
-    y = "v1"
-
-v1()
 print(y)
 
 #list initializing
-list [1,2,3,4]
-print(list())
+list = [1,2,3,4]
+print (list[0:4])
 
 ns=[]
 ns.append(x)
