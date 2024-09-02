e3c6b974097de5c3eb9d5e0f86592889211895e5
Leighton Chen <chenletong19@gmail.com>
2024-09-02 14:01:42 +0800

    update

--- computer_science/denarybinaryconvert/main.py
+++ computer_science/denarybinaryconvert/main.py
@@ -1,11 +1,12 @@
-'''Denary to Binary convert'''
-n = '45'
-s=''
-while True:
-    if n%2==1:
-        s=s+'1'
-    else:
-        s=s+'0'
-
-    n=n//2
+n = int(input("Enter a denary number: "))
+s = ''
+if n == 0:
+    s = '0'
+else:
+    while n > 0:
+        if n % 2 == 1:
+            s = '1' + s
+        else:
+            s = '0' + s
+        n = n // 2
 print(s)
